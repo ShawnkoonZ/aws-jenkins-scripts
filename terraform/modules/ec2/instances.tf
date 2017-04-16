c14277d39648fbd445e5519672a493438466bf30
shawnkoon <shawn9405@gmail.com>
2017-04-15 17:07:50 -0700

    Added user_data script to the terraform jenkins-server istnace

--- terraform/modules/ec2/instances.tf
+++ terraform/modules/ec2/instances.tf
@@ -4,6 +4,15 @@ resource "aws_instance" "shawnkoonz_jenkins_server" {
 	key_name = "${var.key_name}"
 	vpc_security_group_ids = ["${aws_security_group.shawnkoonz_jenkins_sg.id}"]
 
+	user_data = <<-EOF
+							#!/bin/bash
+							sudo yum update -y
+							sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+							sudo rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+							sudo yum install jenkins -y
+							sudo service jenkins start
+							EOF
+							
 	tags {
 		Name = "tf_jenkins_server_ec2"
 	}
